23da6b14386b917504dafdd4069b79982bc244b7
Fahad <m.fahad@naheed.pk>
2020-03-18 13:30:34 +0500

    Initial commit

---
+++ app/src/main/java/com/fahad/fpos/fragment/ProductsFragment.java
@@ -0,0 +1,32 @@
+package com.fahad.fpos.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fahad.fpos.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProductsFragment extends Fragment {
+
+
+    public ProductsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_products, container, false);
+    }
+
+}
